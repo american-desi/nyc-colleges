571fbb002bcdeb1cb67082601aec71e27625d461
Josh <josh@Thomass-MBP-3.home>
2016-05-02 23:46:40 -0400

    added markers and bower components

--- js/app.js
+++ js/app.js
@@ -1,1 +1,87 @@
+var map;
+function initMap() {
+	
+	map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 42.000, lng: -78.644},
+          zoom: 6
+        });
+}
+
+function Map(){
+
+	var self = this;
+
+	self.locations = ko.observableArray([
+
+	]);
+
+	self.searchTerm = [
+
+	];
+
+	self.googleImg = [
+
+	];
+
+	self.wikiLink = [
+
+	];
+
+
+};
+
+
+
+
+var markers = [
+	{
+		title: "Columbia University",
+		lat: 40.8075,
+		lng: -73.9626,
+		cityAddress: "New York, NY 10027",
+		url: "www.columbia.edu"
+	},
+	{
+		title: "Harvard University",
+		lat: 42.3770,
+		lng: -71.1167,
+		streetAddress: "Cambridge, MA 02138",
+		cityAddress: "",
+		url: "www.harvard.edu"
+	},
+	{
+		title: "Queens College",
+		lat: 40.7363,
+		lng: -73.8161,
+		cityAddress: "Flushing, NY 11367",
+		url: "www.qc.cuny.edu"
+	},
+	{
+		title: "University of Connecticut",
+		lat: 41.8077,
+		lng: -72.2540,
+		cityAddress: "Storrs, CT 06269",
+		url: "www.uconn.edu"
+	},
+	{
+		title: "Drexel University",
+		lat: 39.9566,
+		lng: 75.1899,
+		cityAddress: "Philadelphia, PA 19104",
+		url: "www.drexel.edu"
+	}
+];
+
+var ViewModel = function() {
+	// var marker = new google.maps.Marker({
+   // position: myLatLng,
+    //map: map,
+   // title: 'Hello World!'
+  //});
+
+};
+
+ko.applyBindings(new ViewModel());
+
+
 
--- views/app-view.js
+++ views/app-view.js
@@ -1,49 +1,11 @@
 var map;
-
-var colu = {lat: 40.8075, lng: -73.9626};
-var marker;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 40.750, lng: -73.8000},
-      zoom: 10
-  });
-
-
-   var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Columbia University</h1>'+
-      '<div id="bodyContent">'+
-      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      'sandstone rock formation in the southern part of the '+
-      'rock caves and ancient paintings. Uluru is listed as a World '+
-      'Heritage Site.</p>'+
-      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-      '(last visited June 22, 2009).</p>'+
-      '</div>'+
-      '</div>';
-
-
-  
-  marker = new google.maps.Marker({
-      position: colu,
-      map: map,
-      title: 'Columbia University'
-  });
-
-  marker.addListener('click', toggleBounce);
-
-  var infowindow = new google.maps.InfoWindow({
-          content: contentString
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 42.000, lng: -78.644},
+          zoom: 6
         });
- 
-  marker.addListener('click', function() {
-      infowindow.open(map, marker);
-  });
+}
 
-
-};
 
 function toggleBounce() {
       if (marker.getAnimation() !== null) {
@@ -53,47 +15,4 @@ function toggleBounce() {
           setTimeout(function(){ marker.setAnimation(null); }, 750);
       }
 };
-
-
-
-
-
-
-var ViewModel = function() {
-
-  //for listview
-  //for search
-  var $wikiElem = $('#wikipedia-links');
-  var wikiUrl = 'http://en.wikipedia.org/w/api.' +
-      'php?action=opensearch&search=' + "Columbia University"+ '&format=json' +
-      '&callback=wikiCallback';
-
-  var wikiRequestTimeout = setTimeout(function(){
-      $wikiElem.text("failed to get wikipedia resources");
-      }, 8000);
-
-  $.ajax({
-      url: wikiUrl,
-      dataType: "jsonp",
-      // jsonp: "callback",
-      success: function(response) {
-      var articleList = response[1];
-
-      for (var i =0; i < articleList.length;i++){
-          articleStr = articleList[i];
-          var url = 'http://en.wikipedia.org/wiki/' + articleStr;
-          $wikiElem.append('<li><a href="' + url +'">'+
-          articleStr + '</a></li>');
-      };
-
-      clearTimeout(wikiRequestTimeout);
-
-
-        }
-    });
-
-  return false
-
-};
-
 
